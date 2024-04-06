1a25892557541a2b4cb4a663cf1a657ec78fdd0c
lvalics <lvalics@gloobus.it>
2024-04-06 20:43:42 +0300

    /api/ working visually

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# todo/todo_api/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+class HelloWorldApiView(APIView):
+    """
+    View to return "Hello World" response.
+    """
+    def get(self, request, format=None):
+        return Response({"message": "Hello World"}, status=status.HTTP_200_OK)
--- app/app/urls.py
+++ app/app/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# app/app/urls.py : Main urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from api import urls as app_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "api-auth/", include("rest_framework.urls")
+    ),  # http://127.0.0.1:8000/api-auth/
+    path("auth/", include("djoser.urls.jwt")),  # http://127.0.0.1:8000/auth/
+    path("api/", include(app_urls)),  # http://127.0.0.1:8000/api/
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
 ]
